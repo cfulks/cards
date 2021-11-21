239555e18c5d51c21a2635c3adf28780271e2fbb
KingMurazor@gmail.com <KingMurazor@gmail.com>
2021-11-21 12:33:17 -0600

    Updated File

--- Solitaire.js
+++ Solitaire.js
@@ -2,15 +2,14 @@
 import Card from './Card';
 const Card = require("./Card");
 
-const stock = [];
-const stack1 = [];
-const stack2 = [];
-const stack3 = [];
-const stack4 = [];
-const stack5 = [];
-const stack6 = [];
-const stack7 = [];
-
+const stock = [new Card(0, 'null', 'null')];
+const stack1 = [new Card(0, 'null', 'null')];
+const stack2 = [new Card(0, 'null', 'null')];
+const stack3 = [new Card(0, 'null', 'null')];
+const stack4 = [new Card(0, 'null', 'null')];
+const stack5 = [new Card(0, 'null', 'null')];
+const stack6 = [new Card(0, 'null', 'null')];
+const stack7 = [new Card(0, 'null', 'null')];
 
 const createDeck = () =>{
     for(let i = 1; i < 14; i++){
@@ -34,6 +33,7 @@ const createDeck = () =>{
             }
         }
     }
+    stock.slice(0,1);
 }
 
 const shuffleDeck = () => {
@@ -59,31 +59,37 @@ const setStacks = () => {
                 for(let j = 0; j < 2; j++){
                     setCard(stack2);
                 }
+                stock.slice(0,1);
             break;
             case 2:     //stack3
                 for(let j = 0; j < 3; j++){
                     setCard(stack3);
                 }
+                stock.slice(0,1);
             break;
             case 3:     //stack4
                 for(let j = 0; j < 4; j++){
                     setCard(stack4);
                 }
+                stock.slice(0,1);
             break;
             case 4:     //stack5
                 for(let j = 0; j < 5; j++){
                     setCard(stack5);
                 }
+                stock.slice(0,1);
             break;
             case 5:     //stack6
                 for(let j = 0; j < 6; j++){
                     setCard(stack6);
                 }
+                stock.slice(0,1);
             break;
             case 6:     //stack7
                 for(let j = 0; j < 7; j++){
                     setCard(stack7);
                 }
+                stock.slice(0,1);
             break;
             default:
                 console.log("Error making stacks");
@@ -104,7 +110,7 @@ const printCardsInStock = () =>{
     console.log(`There are ${stock.length} cards in the deck`);
     
     for(let i = 0; i < stock.length; i++){
-        console.log(stock[i].value);
+        console.log(`There is a ${stock[i].color} ${stock[i].value} of ${stock[i].suit}`);
     }
     console.log();
 }
