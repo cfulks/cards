754e608319a5ff6c530c6c4fbe08da2587f7955c
zshrestha2 <89435596+zshrestha2@users.noreply.github.com>
2021-12-11 17:14:47 -0600

    Locking player out

--- src/engines/BlackjackEngine.js
+++ src/engines/BlackjackEngine.js
@@ -164,7 +164,8 @@ class Game {
   updateGame = () => {
     this.playerTurn++;
     let bets = [];
-    let counter = 1;
+      let counter = 1;
+      
 
     for (let p in this.players) {
       bets.push({
@@ -194,6 +195,15 @@ class Game {
         /** Source: https://bicyclecards.com/how-to-play/blackjack/
          * When the dealer has served every player, the dealers face-down card is turned up. If the total is 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand. The dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or more cards.
          */
+
+          //locking players out when they go broke
+          for (let p in this.players) {
+              if (this.players[p].bank <= 0) {
+                  this.players[p].socket.disconnect(true);
+              }
+                  
+          }
+
       }
     }
 
